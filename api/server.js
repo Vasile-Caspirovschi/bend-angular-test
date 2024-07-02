2f16fb8489889c30916fa1b0ecfbc36bbe28c6d4
Vasile-Caspirovschi <vasilica.caspirovschi@gmail.com>
2024-07-02 13:00:15 +0300

    added cors to the api

--- api/server.js
+++ api/server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
-app.get('/api/areas', (req, res) => {
-  res.sendFile(path.join(__dirname, 'resources', 'areas.json'));
+app.use(cors());
+app.get("/api/areas", (req, res) => {
+  res.sendFile(path.join(__dirname, "resources", "areas.json"));
 });
 
-app.get('/api/things', (req, res) => {
-  res.sendFile(path.join(__dirname, 'resources', 'things.json'));
+app.get("/api/things", (req, res) => {
+  res.sendFile(path.join(__dirname, "resources", "things.json"));
 });
 
 app.listen(port, () => {
